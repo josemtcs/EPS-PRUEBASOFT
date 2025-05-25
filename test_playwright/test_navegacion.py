6c6efa6d63d7b6ac02f8a299013ea5b6818fef73
josem <jose20030707@gmail.com>
2025-05-25 13:25:42 -0500

    Prueba de worklflow para las tres pruebas

--- test_playwright/test_navegacion.py
+++ test_playwright/test_navegacion.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 
 def test_navegacion():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless= True)
+        browser = p.chromium.launch(headless= False)
         page = browser.new_page()
 
         base_url = "http://127.0.0.1:8000"
